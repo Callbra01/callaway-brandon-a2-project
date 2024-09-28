e8ebd9283dc81e4eadcfb2834eaabb0abf895986
wightkid <103604852+wightkid@users.noreply.github.com>
2024-09-28 16:20:01 -0400

    Add Window Initialization vars and function Add test rectangle for proof of concept Add test input for rectangle debugging

--- callaway-brandon-a2-interactive2d/Game.cs
+++ callaway-brandon-a2-interactive2d/Game.cs
@@ -1,5 +1,6 @@
 ﻿// Include code libraries you need below (use the namespace).
 using System;
+using System.Drawing;
 using System.Numerics;
 
 // The namespace your code is in.
@@ -11,14 +12,27 @@ namespace Game10003
     public class Game
     {
         // Place your variables here:
+        static int windowWidth = 400;
+        static int windowHeight = 400;
+        static int[] windowCenter = [windowWidth/2, windowHeight/2];
+        float rectSize = 50;
+        Color rectColor = new Color(10, 0, 155);
+        float rectSpeed = 100;
+        int colorChangeRate = 20;
+        // Color variables
 
+        // Rectangle Variables
+        Vector4 rectangleTest = new Vector4(windowCenter[0], windowCenter[1], 0, 0);
+        
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
-
+            WindowInitialization(windowWidth, windowHeight);
+            rectangleTest.X = windowCenter[0];
+            rectangleTest.Y = windowCenter[1];
         }
 
         /// <summary>
@@ -26,7 +40,52 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
+            Window.ClearBackground(Color.DarkGray);
+            Console.WriteLine($"{rectSize}");
 
+            DrawBackground();
+            HandleInput();
+        }
+
+
+        // Window Setup and Initialization
+        public void WindowInitialization(int windowWidth, int windowHeight)
+        {
+            Window.SetTitle("callaway-brandon-a2-falling-game");
+            Window.SetSize(windowWidth, windowHeight);
+            Window.TargetFPS = 60;
+        }
+
+        public void DrawBackground()
+        {
+            // Set Rectangle color and place rectangle at center of screen
+            Draw.FillColor = rectColor;
+            Draw.Square(rectangleTest.X - rectSize / 2, rectangleTest.Y - rectSize / 2, rectSize);
+        }
+
+        public void HandleInput()
+        {
+            if (Input.IsKeyboardKeyDown(KeyboardInput.W))
+            {
+                if (rectSize > windowWidth)
+                {
+                    rectSize = 25;
+                    rectangleTest.Z += colorChangeRate;
+                    /*
+                    if (rectColor.R < 255)
+                    {
+                        
+                    }
+                    else
+                    {
+                        rectColor.R = 95;
+                    }
+                    */
+                    rectColor.R += (int)rectangleTest.Z;
+                }
+
+                rectSize += Time.DeltaTime * rectSpeed * 10;
+            }
         }
     }
 }
